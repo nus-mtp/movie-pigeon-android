c79281ad8f92260642a8a70a2140e511118311f6
Guo Mingxuan <carlguo26@gmail.com>
2017-01-19 16:40:33 +0800

    login page commit
    framework of login page initialized

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/MainActivity.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/MainActivity.java
@@ -2,6 +2,8 @@ package org.example.team_pigeon.movie_pigeon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,19 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
-    }
+        final Button BSignIn = (Button) findViewById(R.id.buttonSignIn);
+        final Button BRegister = (Button) findViewById(R.id.buttonRegister);
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+        BSignIn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Check Credentials and respond accordingly
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+                // for testing purpose
+                System.out.println("Sign in button clicked");
+            }
+        });
+
+        BSignIn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Check Credentials and respond accordingly
+
+                // for testing purpose
+                System.out.println("Sign in button clicked");
+            }
+        });
     }
 }
