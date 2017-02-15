8df2281be7cca104db3c443eddcfb1af2767a845
Sheng Xuan <shengxuan14@gmail.com>
2017-02-16 01:19:13 +0800

    Discard function of SearchPageFragment, which is replaced by search view in the home page.

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/SearchPageFragment.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/SearchPageFragment.java
@@ -40,7 +40,6 @@ public class SearchPageFragment extends Fragment {
     private FragmentManager fragmentManager;
     private ArrayList<Movie> movies;
     private Gson gson = new Gson();
-    private SearchTask searchTask;
     private Bundle arguments;
     private MovieListFragment movieListFragment = new MovieListFragment();
 
@@ -70,110 +69,7 @@ public class SearchPageFragment extends Fragment {
                 }
             }
         });
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(etSearch.getText().toString().isEmpty()){
-                    Toast.makeText(getActivity(),"Please input keywords to search", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    searchTask = new SearchTask();
-                    searchTask.execute(etSearch.getText().toString(), getActivity().getIntent().getExtras().getString("Token").trim());
-                }
-            }
-        });
         return view;
     }
-
-    private class SearchTask extends AsyncTask<String, Integer, Void> {
-        private final int SUCCESSFUL = 0;
-        private final int ERROR = 1;
-        private final int NO_RESULT = 2;
-        private final int NO_INTERNET = 3;
-        int status;
-
-        @Override
-        protected Void doInBackground(String... params) {
-            try {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new SearchRequestHttpBuilder(params[0],params[1]).getRequest();
-                Response response = client.newCall(request).execute();
-                if (!response.isSuccessful()) {
-                    Log.i(TAG,"connect failed");
-                    status = ERROR;
-                    throw new IOException("Unexpected code" + response);
-                }
-                //Convert json to Arraylist<Movie>
-                movies = gson.fromJson(response.body().charStream(), new TypeToken<ArrayList<Movie>>() {
-                }.getType());
-
-                if (movies.size() == 0) {
-                    status = NO_RESULT;
-                }
-                else{
-                    status = SUCCESSFUL;
-                }
-                return null;
-            } catch (IOException e) {
-                status = NO_INTERNET;
-                Log.e(TAG, e.getMessage());
-            }
-            return null;
-
-        }
-
-        @Override
-        protected void onPreExecute() {
-            Log.i(TAG, "New search request is initialised");
-            btnSearch.setEnabled(false);
-            return;
-        }
-
-        @Override
-        protected void onPostExecute(Void params) {
-            switch (status) {
-                case SUCCESSFUL:
-                    Log.i(TAG, "Search is completed");
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    arguments = new Bundle();
-                    arguments.putSerializable("movies", movies);
-                    movieListFragment.setArguments(arguments);
-                    fragmentTransaction.replace(R.id.fl_content, movieListFragment);
-                    fragmentTransaction.addToBackStack(null);
-                    fragmentTransaction.commit();
-                    break;
-
-                case ERROR:
-                    Toast.makeText(getContext(), "Connection error, please check your connection", Toast.LENGTH_SHORT).show();
-                    btnSearch.setEnabled(true);
-                    break;
-
-                case NO_RESULT:
-                    Toast.makeText(getContext(), "Sorry, the search has no results", Toast.LENGTH_SHORT).show();
-                    btnSearch.setEnabled(true);
-                    break;
-
-                case NO_INTERNET:
-                    Toast.makeText(getContext(), "Connection error, please make sure that you have Internet connection.", Toast.LENGTH_SHORT).show();
-                    btnSearch.setEnabled(true);
-            }
-
-        }
-    }
-
-    //To create a list of movies for testing purpose
-//    public ArrayList<Movie> testCase(int n){
-//        ArrayList<Movie> movies = new ArrayList<>();
-//        for(int i=0;i<n;i++){
-//            Movie movie = new Movie("movie"+i,i+"");
-//            movie.setPosterURL("https://s-media-cache-ak0.pinimg.com/736x/e8/5f/e6/e85fe6cf9815c33aa93f1656623b4442.jpg");
-//            movie.setGenres("Action, Comedy, Short");
-//            movie.setPlot("In the deadliest incident, 53 people died in one village after an avalanche in Nuristan, a north-eastern Afghan province on the Pakistan border. Thirteen people were also killed in an avalanche in northern Pakistan, nine of them in the town of Chitral. Dozens of houses have been destroyed and people were reported to have frozen to death, trapped in cars. Afghanistan's minister of state natural disasters, Wais Ahmad Barmak, confirmed the number of dead to BBC Afghan. There were also avalanches to the north of the Afghan capital, Kabul. \"Avalanches have buried two entire villages,\" a spokesman for the ministry told news agency AFP of the Barg Matal area in Nuristan. The neighbouring mountainous province of Badakhshan was also badly hit by snow storms.");
-//            movies.add(movie);
-//        }
-//        return movies;
-//    }
-
 
 }
