a0c22b5a25ec4a27548f9d5ddb7a68d601ecda3d
Sheng Xuan <shengxuan14@gmail.com>
2017-02-22 00:16:20 +0800

    Update movie page fragment with new UI and update local list feature.

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/MoviePageFragment.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/MoviePageFragment.java
@@ -3,15 +3,16 @@ package org.example.team_pigeon.movie_pigeon;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
+import android.widget.RatingBar;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,16 +20,22 @@ import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import org.example.team_pigeon.movie_pigeon.eventCenter.AddMovieToMovieListEvent;
+import org.example.team_pigeon.movie_pigeon.eventCenter.DeleteMovieFromMovieListEvent;
+import org.example.team_pigeon.movie_pigeon.eventCenter.UpdateMovieListEvent;
 import org.example.team_pigeon.movie_pigeon.models.Movie;
 import org.example.team_pigeon.movie_pigeon.models.PublicRating;
+import org.example.team_pigeon.movie_pigeon.models.UserBookmark;
+import org.example.team_pigeon.movie_pigeon.models.UserRating;
+import org.greenrobot.eventbus.EventBus;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
 
 /**
  * Created by SHENGX on 2017/2/4.
@@ -45,22 +52,60 @@ public class MoviePageFragment extends Fragment {
     private ImageView image_poster, image_imdb, image_douban, image_trakt;
     private Movie movie;
     private ArrayList<PublicRating> publicRatings;
-    private Button btn_bookmark, btn_rate, btn_submit;
-    private AlertDialog ratingDialog;
-    private AlertDialog.Builder dialogBuilder;
-    private View ratingView;
-    private SeekBar ratingBar;
-    private String scoreToBePost;
+    private RatingBar ratingBar;
+    private MenuItem bookmarkItem;
+    private boolean isBookmarked = false;
+    private int position;
+    private String sourceType;
+    private UserBookmark userBookmark;
+    private UserRating userRating;
+    private EventBus eventBus = EventBus.getDefault();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_movie_page, container, false);
-        ratingView = inflater.inflate(R.layout.dialog_rating, null, false);
         loadViews(view);
         setContent(getArguments());
-        initRatingDialog(ratingView);
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        bookmarkItem = menu.findItem(R.id.action_bookmark);
+        bookmarkItem.setVisible(true);
+        if(movie.getUserBookmark().isEmpty()){
+            isBookmarked = false;
+            setBookmarkIcon(false);
+        }
+        else{
+            isBookmarked = true;
+            setBookmarkIcon(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_bookmark:
+                if(isBookmarked){
+                    MyTask bookmarkTask = new MyTask();
+                    bookmarkTask.execute(UNBOOKMARK, movie.getMovieID());
+                }
+                else{
+                    userBookmark = new UserBookmark(movie.getMovieID());
+                    MyTask bookmarkTask = new MyTask();
+                    bookmarkTask.execute(BOOKMARK, movie.getMovieID());
+                }
+                return true;
+        }
+
+        return false;
+    }
+
 
     private void loadViews(View view) {
         txt_country = (TextView) view.findViewById(R.id.text_movie_page_country_content);
@@ -84,12 +129,13 @@ public class MoviePageFragment extends Fragment {
         row_genres = (TableRow) view.findViewById(R.id.row_genres);
         row_actors = (TableRow) view.findViewById(R.id.row_actors);
         linearLayout_ratings = (LinearLayout) view.findViewById(R.id.linear_ratings);
-        btn_bookmark = (Button) view.findViewById(R.id.button_bookmark);
-        btn_rate = (Button) view.findViewById(R.id.button_rate);
+        ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
     }
 
     private void setContent(Bundle argument) {
         movie = (Movie) argument.getSerializable("movie");
+        position = argument.getInt("position");
+        sourceType = argument.getString("type");
         if (movie != null) {
 
             //Set movie poster
@@ -112,9 +158,9 @@ public class MoviePageFragment extends Fragment {
             setTextOtherwiseSetGone(movie.getGenres(), row_genres, txt_genres);
             setTextOtherwiseSetGone(movie.getActors(), row_actors, txt_actors);
 
-            //Set ratings
+            //Set public ratings
             publicRatings = movie.getPublicRatingses();
-            if (publicRatings == null) {
+            if (publicRatings.isEmpty()) {
                 linearLayout_ratings.setVisibility(View.GONE);
             } else {
                 setRatingsOthersSetGone(publicRatings.get(0).getScore(), score_imdb, image_imdb);
@@ -122,30 +168,19 @@ public class MoviePageFragment extends Fragment {
                 setRatingsOthersSetGone(publicRatings.get(2).getScore(), score_trakt, image_trakt);
             }
 
-            //Set buttons
-            if (!movie.getUserRating().isEmpty()) {
-                btn_rate.setText("Edit Your Rating");
+            //Set Rating Bar
+            if(!movie.getUserRating().isEmpty()){
+                ratingBar.setRating(Float.parseFloat(movie.getUserRating().get(0).getScore())/2);
             }
-            if (!movie.getUserBookmark().isEmpty()) {
-                btn_bookmark.setText("Unbookmark");
-            }
-            btn_bookmark.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    MyTask bookmarkTask = new MyTask();
-                    if (btn_bookmark.getText().equals("Bookmark")) {
-                        bookmarkTask.execute(BOOKMARK, movie.getMovieID());
-                    }
-                    if (btn_bookmark.getText().equals("Unbookmark")) {
-                        bookmarkTask.execute(UNBOOKMARK, movie.getMovieID());
-                    }
-                }
-            });
 
-            btn_rate.setOnClickListener(new View.OnClickListener() {
+            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
                 @Override
-                public void onClick(View v) {
-                    ratingDialog.show();
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    if(fromUser){
+                        userRating = new UserRating(movie.getMovieID(), String.valueOf(rating * 2));
+                        MyTask ratingTask = new MyTask();
+                        ratingTask.execute(POST_RATING, movie.getMovieID(), String.valueOf(rating * 2));
+                    }
                 }
             });
         }
@@ -175,49 +210,13 @@ public class MoviePageFragment extends Fragment {
         }
     }
 
-    private void toggleBookmarkButton() {
-        if (btn_bookmark.getText().equals("Bookmark")) {
-            btn_bookmark.setText("Unbookmark");
+    private void setBookmarkIcon(boolean isBookmarked){
+        if(isBookmarked){
+            bookmarkItem.setIcon(R.drawable.ic_bookmark_full);
         }
-        if (btn_bookmark.getText().equals("Unbookmark")) {
-            btn_bookmark.setText("Bookmark");
+        else{
+            bookmarkItem.setIcon(R.drawable.ic_bookmark_empty);
         }
-    }
-
-    private void initRatingDialog(View view) {
-        dialogBuilder = new AlertDialog.Builder(this.getActivity());
-        dialogBuilder.setView(ratingView);
-        dialogBuilder.setCancelable(true);
-        ratingDialog = dialogBuilder.create();
-        ratingBar = (SeekBar) view.findViewById(R.id.seekbar_rating);
-        text_score = (TextView) view.findViewById(R.id.text_score);
-        btn_submit = (Button) view.findViewById(R.id.button_submit);
-        btn_submit.setEnabled(false);
-        ratingBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                text_score.setText(progress / 10 + "." + progress % 10);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                btn_submit.setEnabled(true);
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                DecimalFormat decimalFormat = new DecimalFormat("0.0");
-                scoreToBePost = decimalFormat.format((double) seekBar.getProgress() / 10);
-            }
-        });
-        btn_submit.setOnClickListener(new Button.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                MyTask postRatingTask = new MyTask();
-                postRatingTask.execute(POST_RATING, movie.getMovieID(), scoreToBePost);
-            }
-        });
     }
 
     //Async thread to handle search request
@@ -228,7 +227,6 @@ public class MoviePageFragment extends Fragment {
         private final int SUCCESSFUL_BOOKMARK = 4;
         private final int NO_INTERNET = 1;
         private int status;
-        private String rating;
         RequestHttpBuilderSingleton requestHttpBuilder = RequestHttpBuilderSingleton.getInstance();
 
         @Override
@@ -276,26 +274,44 @@ public class MoviePageFragment extends Fragment {
                 case SUCCESSFUL_RATE:
                     Log.i(TAG, "Rating is completed");
                     Toast.makeText(getContext(), "Update rating successfully!", Toast.LENGTH_SHORT).show();
-                    btn_rate.setText("Edit Your Rating");
+                    movie.getUserRating().clear();
+                    movie.getUserRating().add(userRating);
+                    eventBus.post(new UpdateMovieListEvent(position, movie));
                     break;
 
                 case SUCCESSFUL_BOOKMARK:
                     Log.i(TAG, "Bookmark is completed");
                     Toast.makeText(getContext(), "Bookmark successfully!", Toast.LENGTH_SHORT).show();
-                    toggleBookmarkButton();
+                    setBookmarkIcon(true);
+                    isBookmarked = true;
+                    movie.getUserBookmark().add(userBookmark);
+                    if(sourceType.equals("bookmark")){
+                        eventBus.post(new AddMovieToMovieListEvent(position, movie));
+                    }
+                    else{
+                        eventBus.post(new UpdateMovieListEvent(position, movie));
+                    }
                     break;
 
                 case SUCCESSFUL_UNBOOKMARK:
                     Log.i(TAG, "Unbookmark is completed");
                     Toast.makeText(getContext(), "Unbookmark successfully!", Toast.LENGTH_SHORT).show();
-                    toggleBookmarkButton();
+                    setBookmarkIcon(false);
+                    isBookmarked = false;
+                    movie.getUserBookmark().clear();
+                    if(sourceType.equals("bookmark")){
+                        Log.i(TAG,"******************************************************" + sourceType.equals("bookmark"));
+                        eventBus.post(new DeleteMovieFromMovieListEvent(position));
+                    }
+                    else{
+                        eventBus.post(new UpdateMovieListEvent(position, movie));
+                    }
                     break;
 
                 case NO_INTERNET:
                     Toast.makeText(getContext(), "Connection error, please make sure that you have Internet connection.", Toast.LENGTH_SHORT).show();
                     break;
             }
-            ratingDialog.dismiss();
 
         }
     }
