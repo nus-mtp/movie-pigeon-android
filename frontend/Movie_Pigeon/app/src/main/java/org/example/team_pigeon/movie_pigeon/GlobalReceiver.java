465102d7422888dafc49ca99849cee8a9f96dc27
Guo Mingxuan <carlguo26@gmail.com>
2017-02-26 17:07:59 +0800

    Updated GlobalReceiver class
    Use switch cases for better runtime.
    Use String instead of String[] to store email and password.

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/GlobalReceiver.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/GlobalReceiver.java
@@ -5,42 +5,52 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Switch;
 
 /**
  * Created by Guo Mingxuan on 11/2/2017.
  */
 
 class GlobalReceiver extends BroadcastReceiver {
+    String emailSignin, passwordSignin;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals("startHomePageActivity")) {
-            // get token from the broadcast
-            Intent homePageIntent = new Intent(context, HomePageActivity.class);
-            // pass token to the new activity
-            Bundle bundle = new Bundle();
-            bundle.putString("Token", intent.getStringExtra("Token"));
-            homePageIntent.putExtras(bundle);
-            context.startActivity(homePageIntent);
-            ((Activity)context).finish();
-        } else if (intent.getAction().equals("automaticSignin")) {
-            // sign-in after registration
-            System.out.println("Registration completed. Proceed to sign in");
-            Bundle bundle = intent.getExtras();
-            String email = bundle.getString("email");
-            String password = bundle.getString("password");
-            String[] emailSignin = new String[1];
-            String[] passwordSignin = new String[1];
-            emailSignin[0] = email;
-            passwordSignin[0] = password;
+        String action = intent.getAction();
+        Bundle bundle;
 
-            String[] signInDetails = new String[2];
-            signInDetails[0] = emailSignin[0];
-            signInDetails[1] = passwordSignin[0];
-            SignInHttpBuilder sBuilder = new SignInHttpBuilder(context);
-            sBuilder.execute(signInDetails);
-        } else if (intent.getAction().equals("UserUpdate")) {
-            System.out.println("User Update received");
-            ((Activity)context).finish();
+        switch (action) {
+            case "startHomePageActivity":
+                // get token from the broadcast
+                Intent homePageIntent = new Intent(context, HomePageActivity.class);
+                // pass token to the new activity
+                bundle = new Bundle();
+                bundle.putString("Token", intent.getStringExtra("Token"));
+                homePageIntent.putExtras(bundle);
+                context.startActivity(homePageIntent);
+                ((Activity) context).finish();
+                break;
+
+            case "automaticSignin":
+                // sign-in after registration
+                System.out.println("Registration completed. Proceed to sign in");
+                bundle = intent.getExtras();
+                String email = bundle.getString("email");
+                String password = bundle.getString("password");
+                emailSignin = email;
+                passwordSignin = password;
+
+                String[] signInDetails = new String[2];
+                signInDetails[0] = emailSignin;
+                signInDetails[1] = passwordSignin;
+                SignInHttpBuilder sBuilder = new SignInHttpBuilder(context);
+                sBuilder.execute(signInDetails);
+                break;
+
+            case "UserUpdate":
+                System.out.println("User Update received");
+                ((Activity) context).finish();
+                break;
         }
     }
 }
