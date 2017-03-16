2dddbecbb0c467b0e2ab952ba35099017c034d54
Sheng Xuan <shengxuan14@gmail.com>
2017-03-16 20:41:01 +0800

    Change MovieListAdapter so that it can identify header of a list of movie.

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/adapters/MovieListAdapter.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/adapters/MovieListAdapter.java
@@ -1,11 +1,14 @@
 package org.example.team_pigeon.movie_pigeon.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -16,21 +19,41 @@ import org.example.team_pigeon.movie_pigeon.configs.ImageConfig;
 import org.example.team_pigeon.movie_pigeon.models.Movie;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class MovieListAdapter extends BaseAdapter {
+    private static final int TYPE_MOVIE = 0;
+    private static final int TYPE_SEPARATOR = 1;
     private ArrayList<Movie> movieList;
+    private TreeSet separatorSet = new TreeSet();
     private Context mContext;
     private Movie movie;
     private ImageLoader imageLoader = ImageLoader.getInstance();
     private DisplayImageOptions options = new ImageConfig().getDisplayImageOption();
 
-    public MovieListAdapter(ArrayList<Movie>movieList,Context mContext) {
+    public MovieListAdapter(ArrayList<Movie> movieList, Context mContext) {
         this.mContext = mContext;
         this.movieList = movieList;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return separatorSet.contains(position) ? TYPE_SEPARATOR : TYPE_MOVIE;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
     public void addListItemToAdapter(ArrayList<Movie> list) {
         movieList.addAll(list);
+        this.notifyDataSetChanged();
+    }
+
+    public void addHeaderToAdapter(final Movie separator) {
+        movieList.add(separator);
+        separatorSet.add(movieList.size() - 1);
         this.notifyDataSetChanged();
     }
 
@@ -40,12 +63,12 @@ public class MovieListAdapter extends BaseAdapter {
     }
 
     public void addMovieItemToAdapter(Movie movie, int position) {
-        movieList.add(position,movie);
+        movieList.add(position, movie);
         this.notifyDataSetChanged();
     }
 
     public void updateMovieItemToAdapter(Movie movieNew, int position) {
-        movieList.set(position,movieNew);
+        movieList.set(position, movieNew);
         this.notifyDataSetChanged();
     }
 
@@ -66,27 +89,36 @@ public class MovieListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
+        movie = movieList.get(position);
+        ViewHolder viewHolder = null;
+        int type = getItemViewType(position);
         if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.movie_list_item, parent, false);
             viewHolder = new ViewHolder();
-            viewHolder.txt_title = (TextView) convertView.findViewById(R.id.text_movie_list_title);
-            viewHolder.image_poster = (ImageView) convertView.findViewById(R.id.image_movie_list_poster);
+            switch (type) {
+                case TYPE_MOVIE:
+                    convertView = LayoutInflater.from(mContext).inflate(R.layout.movie_list_item, null);
+                    viewHolder.txt_title = (TextView) convertView.findViewById(R.id.text_movie_list_title);
+                    viewHolder.image_poster = (ImageView) convertView.findViewById(R.id.image_movie_list_poster);
+                    break;
+                case TYPE_SEPARATOR:
+                    convertView = LayoutInflater.from(mContext).inflate(R.layout.header_all_search_list, null);
+                    convertView.setClickable(false);
+                    break;
+            }
             convertView.setTag(viewHolder);
-        }
-        else {
+        } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        movie = movieList.get(position);
-        viewHolder.txt_title.setText(movie.getTitle());
-        imageLoader.displayImage(movieList.get(position).getPosterURL(),viewHolder.image_poster,options);
-
+        if (viewHolder.txt_title != null) {
+            viewHolder.txt_title.setText(movie.getTitle());
+            imageLoader.displayImage(movieList.get(position).getPosterURL(), viewHolder.image_poster, options);
+        }
 
         return convertView;
     }
 
-        private static class ViewHolder {
-            TextView txt_title;
-            ImageView image_poster;
-        }
+    private static class ViewHolder {
+        TextView txt_title;
+        ImageView image_poster;
     }
+}
