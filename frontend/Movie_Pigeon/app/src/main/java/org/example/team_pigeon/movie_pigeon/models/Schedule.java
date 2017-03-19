c0c1cd9528043f26cc0b57f6940d0918056fc3c9
Sheng Xuan <shengxuan14@gmail.com>
2017-03-19 19:11:26 +0800

    Add attributes into Schedule class.

--- frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/models/Schedule.java
+++ frontend/Movie_Pigeon/app/src/main/java/org/example/team_pigeon/movie_pigeon/models/Schedule.java
@@ -4,6 +4,8 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by SHENGX on 2017/3/4.
@@ -14,12 +16,23 @@ public class Schedule implements Serializable{
     private String movieId;
     @Expose
     @SerializedName("cinema_id")
-    private String cinemaId;
+    private int cinemaId;
     @Expose
     @SerializedName("schedule")
     private String time;
     @Expose
+    @SerializedName("time")
+    private String showtime;
+    @Expose
     private String type;
+    @Expose
+    @SerializedName("date")
+    private String date;
+    @Expose
+    @SerializedName("cinema_name")
+    private String cinemaName;
+
+    private ArrayList<String> showTimeArray;
 
     public String getMovieId() {
         return movieId;
@@ -29,11 +42,11 @@ public class Schedule implements Serializable{
         this.movieId = movieId;
     }
 
-    public String getCinemaId() {
+    public int getCinemaId() {
         return cinemaId;
     }
 
-    public void setCinemaId(String cinemaId) {
+    public void setCinemaId(int cinemaId) {
         this.cinemaId = cinemaId;
     }
 
@@ -51,5 +64,37 @@ public class Schedule implements Serializable{
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+
+    public ArrayList<String> getShowTimeArray() {
+        return showTimeArray;
+    }
+
+    public void setShowTimeArray(ArrayList<String> showTimeArray) {
+        this.showTimeArray = showTimeArray;
+    }
+
+    public String getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(String showtime) {
+        this.showtime = showtime;
     }
 }
